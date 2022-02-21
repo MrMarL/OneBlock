e3be700c628b34ba29d9a2c12457de95cc4343ae
Mark <61845375+MrMarL@users.noreply.github.com>
2022-02-21 22:26:04 +0300

    0.9.7 pre

--- src/Oneblock/OBWorldGuard.java
+++ src/Oneblock/OBWorldGuard.java
@@ -3,7 +3,7 @@ package Oneblock;
 import org.bukkit.util.Vector;
 
 public abstract class OBWorldGuard {
-	public final boolean canUse = true;
+	public final boolean canUse = false;
 	public final String regionName = "OB_WG_%d";
 	
 	public abstract boolean CreateRegion(String pl, Vector coord1, Vector coord2, int id) ;
