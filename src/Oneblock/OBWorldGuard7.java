50f1f94453db0f8ef02611e6565ad8e93f46ad4a
Mark <61845375+MrMarL@users.noreply.github.com>
2022-03-12 16:31:23 +0300

    add empty worldguard to repo

---
+++ src/Oneblock/OBWorldGuard7.java
@@ -0,0 +1,22 @@
+package Oneblock;
+
+import org.bukkit.util.Vector;
+
+public class OBWorldGuard7 extends OBWorldGuard{
+	
+	public boolean CreateRegion(String pl, Vector coord1, Vector coord2, int id) {
+		return true;
+	}
+	
+	public boolean addMember(String pl, int id) {
+		return true;
+	}
+	
+	public boolean removeMember(String pl, int id) {
+		return true;
+	}
+	
+	public boolean RemoveRegions(int id) {
+		return true;
+	}
+}
---
+++ src/com/sk89q/worldedit/BlockVector.java
@@ -0,0 +1,7 @@
+package com.sk89q.worldedit;
+
+import org.bukkit.util.Vector;
+
+public class BlockVector extends Vector {
+    public BlockVector(int x, int y, int z) {super(x, y, z);}
+}
