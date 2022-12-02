deea6b97957776be8a48fe9b9dc26181aa862e1c
Mark <61845375+MrMarL@users.noreply.github.com>
2022-12-03 01:43:18 +0300

    ~

--- src/Oneblock/gui/GUIListener.java
+++ src/Oneblock/gui/GUIListener.java
@@ -1,18 +1,13 @@
 package Oneblock.gui;
 
-import java.util.List;
-
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-
-import Oneblock.ChestItems;
 
 public class GUIListener implements Listener {
 
