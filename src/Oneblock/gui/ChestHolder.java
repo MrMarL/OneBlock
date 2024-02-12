eb8524853bc6f453cc17c9750afa28468215f359
Mark <61845375+MrMarL@users.noreply.github.com>
2024-02-12 18:42:16 +0300

    Create ChestHolder.java

---
+++ src/Oneblock/gui/ChestHolder.java
@@ -0,0 +1,12 @@
+package Oneblock.gui;
+
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+
+public class ChestHolder implements InventoryHolder {
+
+	@Override
+	public Inventory getInventory() {
+		return null;
+	}
+}
