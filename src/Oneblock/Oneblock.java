58cb65434feb8001e75e7a34a9f9f3f29257f27b
Mark <61845375+MrMarL@users.noreply.github.com>
2024-09-21 12:24:48 +0300

    1.2.4 - update void world gen (set biome to PLAINS))

--- src/Oneblock/Oneblock.java
+++ src/Oneblock/Oneblock.java
@@ -1107,7 +1107,7 @@ public class Oneblock extends JavaPlugin {
     		        	"  ▄▄    ▄▄",
     		        	"█    █  █▄▀",
     		        	"▀▄▄▀ █▄▀",
-    		        	"Create by MrMarL\nPlugin version: v1.2.3",
+    		        	"Create by MrMarL\nPlugin version: v1.2.4",
     		        	"Server version: ", superlegacy?"super legacy":(legacy?"legacy":""), XMaterial.getVersion()));
     		        return true;
 		    }
--- src/Oneblock/VoidChunkGenerator.java
+++ src/Oneblock/VoidChunkGenerator.java
@@ -5,22 +5,29 @@ import java.util.List;
 import java.util.Random;
 
 import org.bukkit.World;
+import org.bukkit.block.Biome;
 import org.bukkit.generator.BlockPopulator;
 import org.bukkit.generator.ChunkGenerator;
 
 @SuppressWarnings("deprecation")
 public class VoidChunkGenerator extends ChunkGenerator {
-	private ChunkData nullData = null;
-
-	@Override
-	public List<BlockPopulator> getDefaultPopulators(World world) {
-		return Collections.<BlockPopulator>emptyList();
+	public ChunkData generateChunks(World world) {
+		return createChunkData(world);
 	}
 
 	@Override
-	public ChunkData generateChunkData(World world, Random random, int chunkX, int chunkZ, BiomeGrid biome) {
-		if (nullData != null)
-			return nullData;
-		return nullData = super.createChunkData(world);
-	}
+    public ChunkData generateChunkData(World world, Random random, int chunkX, int chunkZ, BiomeGrid biomes) {
+        ChunkData chunkData = createChunkData(world);
+
+        for (int x = 0; x < 16; x++) 
+            for (int z = 0; z < 16; z++) 
+                biomes.setBiome(x, z, Biome.PLAINS);
+
+        return chunkData;
+    }
+
+    @Override
+    public List<BlockPopulator> getDefaultPopulators(final World world) {
+        return Collections.emptyList();
+    }
 }
